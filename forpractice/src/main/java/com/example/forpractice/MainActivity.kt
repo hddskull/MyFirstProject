bba17d5b79a89b687af1e4adcbed27b7e21c8bf3
Max <maxdgladkov@gmail.com>
2022-02-23 23:34:34 +0300

    practical assignmetn for module 2.2

--- forpractice/src/main/java/com/example/forpractice/MainActivity.kt
+++ forpractice/src/main/java/com/example/forpractice/MainActivity.kt
@@ -2,40 +2,59 @@ package com.example.forpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Gravity
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val NCREATE  = "Activity CREATED"
+    private val NSTART   = "Activity STARTED"
+    private val NRESUME  = "Activity RESUME"
+    private val NPAUSE   = "Activity PAUSED"
+    private val NSTOP    = "Activity STOPPED"
+    private val NRESTART = "Activity RESTARTED"
+    private val NDESTROY = "Activity DESTROYED"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        showMessage("I created")
+        var toast = Toast.makeText(this, NCREATE,Toast.LENGTH_SHORT)
+        toast.show()
     }
-
-    override fun onPause() {
-        super.onPause()
-        showMessage("I paused")
-    }
-
     override fun onStart() {
         super.onStart()
-        showMessage("I started")
+        var toast = Toast.makeText(this, NSTART,Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.LEFT, 0 ,0)
+        toast.show()
     }
-
+    override fun onResume() {
+        super.onResume()
+        var toast = Toast.makeText(this, NRESUME,Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0 ,0)
+        toast.show()
+    }
+    override fun onPause() {
+        super.onPause()
+        var toast = Toast.makeText(this, NPAUSE,Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.RIGHT, 0 ,0)
+        toast.show()
+    }
+    override fun onStop() {
+        super.onStop()
+        var toast = Toast.makeText(this, NSTOP,Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.CENTER, 0 ,0)
+        toast.show()
+    }
     override fun onRestart() {
         super.onRestart()
-        showMessage("I restarted")
+        var toast = Toast.makeText(this, NRESTART,Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.FILL_HORIZONTAL, 0 ,0)
+        toast.show()
     }
-
     override fun onDestroy() {
         super.onDestroy()
-        showMessage("I destroyed")
-    }
-
-    private val TAG = "condition"
-
-    fun showMessage(s: String){
-        Toast.makeText(this,s,Toast.LENGTH_SHORT).show()
-        Log.d(TAG, s)
+        var toast = Toast.makeText(this, NDESTROY,Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.FILL, 0 ,0)
+        toast.show()
     }
 }
